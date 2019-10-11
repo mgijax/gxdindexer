f7dcf046fa4a28ef1da53721cb6f9d6543f14d58
Jonathan Beal <jsb@jax.org>
2019-10-11 11:12:19 -0400

    removed references to defunct gxddatamodel product (was using it for the also-defunct gxdMarker index)

--- src/org/jax/mgi/shr/Fetcher.java
+++ src/org/jax/mgi/shr/Fetcher.java
@@ -10,8 +10,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.apache.log4j.Logger;
 
-import org.jax.mgi.gxddatamodel.GxdMarker;
-
 // provides methods for fetching certain types of standard data, as a
 // convenience to all the GXD indexers
 public class Fetcher {
@@ -19,7 +17,6 @@ public class Fetcher {
 	private Logger log = Logger.getLogger(SQLExecutor.class);
 	private SQLExecutor sql;
 	private Map<String,VocabTermCache> vocabs = new HashMap<String,VocabTermCache>();
-	private Map<String,GxdMarker> markers;
 	private Map<String,List<String>> markerSynonyms;
 	
 	// hide the default constructor
@@ -34,7 +31,6 @@ public class Fetcher {
 	// clear any cached data sets
 	public void clearCaches() {
 		this.vocabs = new HashMap<String,VocabTermCache>();
-		this.markers = null;
 		this.markerSynonyms = null;
 		log.info("Cleared caches in " + this.toString());
 	}
@@ -68,57 +64,6 @@ public class Fetcher {
                 rs.close();
                 log.info(" - Cached synonyms for " + this.markerSynonyms.size() + " markers");
 		return this.markerSynonyms;
-	}
-
-	// get a GxdMarker object for each non-withdrawn mouse marker (for
-	// markers with expression-related data (classical, RNA-Seq, lit) )
-	public Map<String,GxdMarker> getMouseMarkers() throws SQLException {
-		// if we already looked them up, just return from cache
-		if (this.markers != null) {
-			return this.markers;
-		}
-
-		this.markers = new HashMap<String,GxdMarker>();
-
-                String cmd = "select m.marker_key, m.symbol, m.name, m.primary_id, "
-                        + " m.marker_type, m.marker_subtype, "
-                        + " coalesce(coord.chromosome, cm.chromosome, band.chromosome) as chromosome, "
-                        + " coord.start_coordinate::text, coord.end_coordinate::text, coord.strand, "
-                        + " cm.cm_offset::text, band.cytogenetic_offset "
-                        + "from marker m "
-                        + "left outer join marker_location coord on ( "
-                        + " m.marker_key = coord.marker_key "
-                        + " and coord.location_type = 'coordinates') "
-                        + "left outer join marker_location cm on ( "
-                        + " m.marker_key = cm.marker_key "
-                        + " and cm.location_type = 'centimorgans') "
-                        + "left outer join marker_location band on ( "
-                        + " m.marker_key = band.marker_key "
-                        + " and band.location_type = 'cytogenetic') "
-                        + "where m.organism = 'mouse' "
-                        + " and m.status != 'withdrawn' ";
-
-                log.info("About to retrieve GxdMarkers");
-                ResultSet rs = this.sql.executeProto(cmd);
-
-                while (rs.next()) {
-                        GxdMarker marker = new GxdMarker();
-                        marker.setMarkerKey(rs.getInt("marker_key"));
-                        marker.setSymbol(rs.getString("symbol"));
-                        marker.setName(rs.getString("name"));
-                        marker.setPrimaryID(rs.getString("primary_id"));
-                        marker.setMarkerType(rs.getString("marker_type"));
-                        marker.setMarkerSubType(rs.getString("marker_subtype"));
-                        marker.setChromosome(rs.getString("chromosome"));
-                        marker.setStartCoord(rs.getString("start_coordinate"));
-                        marker.setEndCoord(rs.getString("end_coordinate"));
-                        marker.setStrand(rs.getString("strand"));
-                        marker.setCentimorgans(rs.getString("cm_offset"));
-                        this.markers.put(marker.getPrimaryID(), marker);
-                }
-                rs.close();
-                log.info(" - Cached " + this.markers.size() + " markers");
-		return this.markers;
 	}
 
 	// get an ordered list of keys for official mouse markers that have
