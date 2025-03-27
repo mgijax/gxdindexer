e6c0338e3c01c10b7de352ecf3ce177e9e1681fa
Joel Richardson <joel.richardson@jax.org>
2025-03-27 10:16:32 -0400

    e4g-180 Add #of Bioreplicates to the Heat Map

--- src/main/java/org/jax/mgi/gxdindexer/GxdConsolidatedSampleIndexer.java
+++ src/main/java/org/jax/mgi/gxdindexer/GxdConsolidatedSampleIndexer.java
@@ -19,13 +19,20 @@ public class GxdConsolidatedSampleIndexer extends Indexer {
 
 	public void index() throws Exception {
 		List<SolrInputDocument> docs = new ArrayList<SolrInputDocument>();
-		String cmd = "select s.consolidated_sample_key, t.primary_id as structure_exact, " +
-		" s.theiler_stage, g.background_strain, g.combination_1 as genotype, " + 
-			" e.primary_id as assay_id, s.age, t.term as printname, s.sex " + 
-		"from expression_ht_consolidated_sample s	"
-			+ "inner join genotype g on (s.genotype_key = g.genotype_key) " + 
-		"inner join expression_ht_experiment e on (s.experiment_key = e.experiment_key) " + 
-			"inner join term t on (s.emapa_key = t.term_key)";
+		String cmd =
+			"with counts as ( " +
+			"select sm.consolidated_sample_key, count(*) as bioreplicateCount " +
+			"from expression_ht_sample_map sm " +
+			"group by  sm.consolidated_sample_key " +
+			") " +
+			"select s.consolidated_sample_key, t.primary_id as structure_exact, " +
+			" s.theiler_stage, g.background_strain, g.combination_1 as genotype, " + 
+			" e.primary_id as assay_id, s.age, t.term as printname, s.sex, c.bioreplicateCount " + 
+			"from expression_ht_consolidated_sample s	" +
+			"inner join genotype g on (s.genotype_key = g.genotype_key) " + 
+			"inner join expression_ht_experiment e on (s.experiment_key = e.experiment_key) " + 
+			"inner join term t on (s.emapa_key = t.term_key) " +
+			"inner join counts c on (s.consolidated_sample_key = c.consolidated_sample_key)";
 
 		ResultSet rs = ex.executeProto(cmd);
 		while (rs.next()) {
@@ -39,6 +46,7 @@ public class GxdConsolidatedSampleIndexer extends Indexer {
 			doc.addField(GxdResultFields.AGE, rs.getString("age"));
 			doc.addField(GxdResultFields.STRUCTURE_PRINTNAME, rs.getString("printname"));
 			doc.addField(GxdResultFields.SEX, rs.getString("sex"));
+			doc.addField(GxdResultFields.BIOREPLICATE_COUNT, rs.getString("bioreplicateCount"));
 			docs.add(doc);
 		}
 		writeDocs(docs);
