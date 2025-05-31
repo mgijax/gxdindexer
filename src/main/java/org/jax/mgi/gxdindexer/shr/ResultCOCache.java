8d65663f1e58b0686301026c8a06c7a3af9d60d9
PeteFrost <pete.frost@jax.org>
2025-05-31 13:30:09 -0400

    Removal of top level term CELL

--- src/main/java/org/jax/mgi/gxdindexer/shr/ResultCOCache.java
+++ src/main/java/org/jax/mgi/gxdindexer/shr/ResultCOCache.java
@@ -6,11 +6,12 @@ public class ResultCOCache extends TermAssociationCache {
 	// initialize this cache upon instantiation of the object, propagating any Exception
 	// raised in the initialization process
 	public ResultCOCache() throws Exception {
-		String cmd = "select ct.result_key as object_key, t2.term as term "
+		String cmd = "select ct.result_key as object_key, tth.label as term "
 			+ "from expression_result_cell_type ct, term t1, term_to_header tth, term t2 "
 			+ "where ct.cell_type = t1.term "
 			+ " and t1.term_key = tth.term_key "
 			+ " and tth.header_term_key = t2.term_key "
+			+ " and tth.accid != 'CL:0000000' "
 			+ "order by 1, 2";
 		this.populate(cmd);
 	}
